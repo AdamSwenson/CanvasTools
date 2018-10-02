5234e21263f176854f65555258e2317c874f7f07
AdamSwenson <adam.swenson@csun.edu>
2018-10-02 15:27:38 -0700

    journals tools working better now

--- CanvasTools/RequestTools.py
+++ CanvasTools/RequestTools.py
@@ -5,12 +5,18 @@ __author__ = 'adam'
 
 import requests
 import json
+from CanvasTools.UrlTools import make_url
 
 import environment
 
-def send_get_request(url):
+
+def make_request_header():
+    """Creates the request header with Authorization value expected by canvas"""
+    return {'Authorization': 'Bearer {}'.format(environment.TOKEN)}
+
+def send_get_request(url, data={}):
     head = {'Authorization': 'Bearer {}'.format(environment.TOKEN)}
-    response = requests.get(url, headers=head)
+    response = requests.get(url, headers=make_request_header(), json=data)
     return response.json()
 
 def send_post_request(url, data):
@@ -18,6 +24,23 @@ def send_post_request(url, data):
     response = requests.post(url, headers=head, json=data)
     return response.json()
 
+
+# Assignments
+# Get assignment list
+def get_all_course_assignments(course_id):
+    """Returns a list of all the assignments for the course
+    Uses api: GET /api/v1/courses/:course_id/assignments
+    """
+    url = make_url(course_id, 'assignments')
+    return send_get_request(url, {'include': 'submissions'})
+
+
+def get_assignment(course_id, assignment_id):
+    """Retrieves the specified assignment and returns it as a dictionary"""
+    url = make_url(course_id, 'assignments')
+    url = "%s/%s" % (url, assignment_id)
+    #     print(url)
+    return send_get_request(url)
 
 
 if __name__ == '__main__':
--- environment.py
+++ environment.py
@@ -11,23 +11,23 @@ import configparser
 ROOT = os.getenv( "HOME" )
 # The folder containing environment.py
 PROJ_BASE = os.path.abspath(os.path.dirname(__file__))
-
 # Folders outside of the project foler
 enclosing = os.path.abspath(os.path.dirname(PROJ_BASE))
 
+############### Credentials
 # All login credentials are defined in files here.
 # THIS CONTENTS OF THIS FOLDER MUST NOT BE COMMITTED TO VERSION CONTROL!
 CREDENTIALS_FOLDER_PATH = "%s/private" % PROJ_BASE
-
-TEMP_DATA_PATH = "%s/temp" % PROJ_BASE
-
-############### Credentials
 CREDENTIALS_FILE = "%s/canvas-credentials.ini" % CREDENTIALS_FOLDER_PATH
 
-print("Reading credentials from %s" % CREDENTIALS_FILE)
+print("Reading credentials and settingsfrom %s" % CREDENTIALS_FILE)
 config = configparser.ConfigParser()
 config.read(CREDENTIALS_FILE)
 
 TOKEN = config['credentials'].get('TOKEN')
 URL_BASE = config['url'].get('BASE')
+
+TEMP_DATA_PATH = "%s/temp" % PROJ_BASE
+ARCHIVE_FOLDER = "%s/%s" % (ROOT, config['folders'].get('STUDENT_WORK_ARCHIVE_FOLDER'))
+DATA_FOLDER = '%s/data' % PROJ_BASE
 
